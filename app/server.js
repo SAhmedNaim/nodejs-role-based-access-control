da97db10e166b9dc4c5c819af3b15a631742a204
s-ahmed-naim <naim.ahmed035@gmail.com>
2021-07-11 20:52:27 +0600

    Connection established with Database

--- app/server.js
+++ app/server.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+require("dotenv").config({
+    path: path.join(__dirname, "../.env")
+});
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+};
+
+mongoose.connect(`mongodb://${process.env.DB_HOST}:27017/${process.env.DB_NAME}`, options).then(() => {
+    console.log('Connection with Database has been established successfully')
+});
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "S Ahmed Naim"
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on Port: ${PORT}`)
