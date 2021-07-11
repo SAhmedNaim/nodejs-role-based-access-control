3b93548a60d7505535f6d3bf89c2244eade21a8c
s-ahmed-naim <naim.ahmed035@gmail.com>
2021-07-11 21:29:06 +0600

    Register User

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,6 +1,22 @@
 const User = require('../models/userModel');
 
-exports.getUsers = async (req, res, next) => {
+exports.register = async (req, res, next) => {
+    try {
+        const { role, email, password } = req.body;
+        const newUser = new User({ email, password, role: role || "basic" });
+
+        await newUser.save();
+
+        res.json({
+            data: newUser,
+            message: "You have signed up successfully"
+        })
+    } catch (error) {
+        next(error)
+    }
+};
+
+exports.getUsers = async (req, res) => {
     const users = await User.find({});
     res.status(200).json({
         data: users
--- app/routes/route.js
+++ app/routes/route.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+router.post('/register', userController.register);
+
 router.get("/users", userController.getUsers);
 
 module.exports = router;
