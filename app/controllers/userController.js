918ae76245f33fbc41b4473823e18e25d0c9e1b2
s-ahmed-naim <naim.ahmed035@gmail.com>
2021-07-11 21:43:01 +0600

    Fetch User by ID

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -22,3 +22,17 @@ exports.getUsers = async (req, res) => {
         data: users
     });
 };
+
+exports.getUser = async (req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        const user = await User.findById(userId);
+        if (!user) return next(new Error('User does not exist'));
+        res.status(200).json({
+            data: user
+        });
+    } catch (error) {
+        console.log(error.toString())
+        next(error)
+    }
+};
--- app/routes/route.js
+++ app/routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/register', userController.register);
-
 router.get("/users", userController.getUsers);
+router.get('/users/:userId', userController.getUser);
 
 module.exports = router;
